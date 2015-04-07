6e8c019bef53009de525330e2ee66861dfaf42b1
Mitch <hello@liquidvisual.com>
2015-04-07 13:01:47 +1000

    First Commit

---
+++ src/scripts/main.js
@@ -0,0 +1,123 @@
+/*
+    MAIN SCRIPTS - Last updated: 00-00-00
+*/
+//-----------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------
+
+var TOUCH_ENABLED = $(".touch").length;
+
+//-----------------------------------------------------------------
+// Document Ready
+//-----------------------------------------------------------------
+
+$(document).ready(function() {
+    NProgress.start(); // Start preloader bar
+});
+
+$(window).load(function() {
+    NProgress.done();
+});
+
+// window.onload = function(){
+//     // NProgress.done();
+// }
+
+//-----------------------------------------------------------------
+// Offcanvas Menu
+//-----------------------------------------------------------------
+
+$("#off-canvas-menu").mmenu({ "offCanvas": { "position": "right" }});
+
+//-----------------------------------------------------------------
+// Testimonial Slider
+//-----------------------------------------------------------------
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
+
+//-----------------------------------------------------------------
+// Kickstart Foundation / Touch Conditionals
+//-----------------------------------------------------------------
+
+// var touchEvent = TOUCH_ENABLED ? "touchstart" : "click";
+
+//Trigger hamburger by touch on mobile - this eliminates glitch with FastClick.js
+$('.hamburger').bind('click', function(e) {
+    e.preventDefault();
+    $('#off-canvas-menu').trigger('open.mm');
+});
+
+if (TOUCH_ENABLED) {
+    // Make Accordion jump to the top of the heading on mobile
+    // http://foundation.zurb.com/forum/posts/1316-accordion-jump-to-top-when-active
+    /*$(document).foundation('accordion', {
+        callback: function (el){
+            var containerPos = $(el).parent().offset().top;
+            $('html, body').animate({ scrollTop: containerPos }, 300);
+        }
+    });*/
+}
+//-----------------------------------------------------------------
+// <= IE8 Caution Message
+//-----------------------------------------------------------------
+
+//$('.lv-alert .close-btn').click(function(){$(this).parent().hide();});
+
+//-----------------------------------------------------------------
+// +++ HELPERS +++
+//-----------------------------------------------------------------
+//-----------------------------------------------------------------
+// Screen Data Tool
+//-----------------------------------------------------------------
+
+function screenData() {
+    if ($('.lv-screen-data').length != 1) {
+        $('.lv-super').append('<div class="lv-screen-data"></div>');
+    }
+    var screenWidth = $(window).width();
+    var screenHeight = $(window).height();
+    var device;
+
+    if (screenWidth <= 640) device = "Small";
+    if (screenWidth > 640 && screenWidth <= 1024) device = "Medium";
+    if (screenWidth > 1024 && screenWidth <= 1280) device = "Large";
+    if (screenWidth > 1280) device = "xLarge";
+
+    $('.lv-screen-data').html(screenWidth+" x "+screenHeight+"<br>"+device)
+        .css({
+            'position': 'fixed',
+            'top': 0,
+            'padding': '10px 20px',
+            'background': 'rgba(0,0,0,0.5)',
+            'font-family': 'Helvetica Neue',
+            'font-size': '14px',
+            'color': 'white',
+            'z-index': 9000
+        });
+}
+
+screenData();
+
+$(window).resize(function(){
+    screenData();
+});
+
+//==================================================
+// Developer: COMMAND+S for screen width
+//==================================================
+
+$(document).keypress(function(event) {
+    if (event.which == 115 && (event.ctrlKey||event.metaKey)||(event.which == 19)) {
+        event.preventDefault();
+        alert("(w) "+$(window).width()+" (h) "+$(window).height());
+        return false;
+    }
+    return true;
+});
+//==================================================
+//
+//==================================================
---
+++ src/scripts/scroll-highlight.js
@@ -0,0 +1,76 @@
+/*
+    SCROLL HIGHLIGHT - Last updated: 31.03.15
+
+    - Notes -
+
+     An alternative to Foundation's Magellan
+     http://jsfiddle.net/mekwall/up4nu/
+*/
+//-----------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------
+
+var lastId;
+var topMenu = $(".js-scroll-highlight");
+var topMenuHeight = topMenu.outerHeight()+15;
+var menuItems = topMenu.find("a"); // All list items
+
+//-----------------------------------------------------------------
+//
+//-----------------------------------------------------------------
+
+// Anchors corresponding to menu items
+var scrollItems = menuItems.map(function(){
+	var item = $($(this).attr("href"));
+	if (item.length) return item;
+});
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href");
+  var offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+
+  $('html, body').stop().animate({ scrollTop: offsetTop }, 300);
+  e.preventDefault();
+});
+
+//-----------------------------------------------------------------
+// Bind to scroll
+//-----------------------------------------------------------------
+
+$(window).scroll(function(event){
+
+   var fromTop = $(this).scrollTop()+topMenuHeight; // Get container scroll position
+   var y = $(this).scrollTop();
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+
+	// Magic numbers ahead - height of sticky nav
+   if (y >= 36)
+       $('.js-global-header').addClass('is-sticky');
+   else
+       $('.js-global-header').removeClass('is-sticky');
+
+
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }
+});
+
+//==================================================
+//
+//==================================================
