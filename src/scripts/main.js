54fec67264c7374aed7278c3e8200801ca6578e1
Mitch <hello@liquidvisual.com>
2015-05-15 02:32:39 +1000

    Sass re-structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -70,42 +70,6 @@ if (TOUCH_ENABLED) {
 //-----------------------------------------------------------------
 // +++ HELPERS +++
 //-----------------------------------------------------------------
-//-----------------------------------------------------------------
-// Screen Data Tool
-//-----------------------------------------------------------------
-
-function screenData() {
-    if ($('.lv-screen-data').length != 1) {
-        $('.lv-super').append('<div class="lv-screen-data"></div>');
-    }
-    var screenWidth = $(window).width();
-    var screenHeight = $(window).height();
-    var device;
-
-    if (screenWidth <= 640) device = "Small";
-    if (screenWidth > 640 && screenWidth <= 1024) device = "Medium";
-    if (screenWidth > 1024 && screenWidth <= 1280) device = "Large";
-    if (screenWidth > 1280) device = "xLarge";
-
-    $('.lv-screen-data').html(screenWidth+" x "+screenHeight+"<br>"+device)
-        .css({
-            'position': 'fixed',
-            'top': 0,
-            'padding': '10px 20px',
-            'background': 'rgba(0,0,0,0.5)',
-            'font-family': 'Helvetica Neue',
-            'font-size': '14px',
-            'color': 'white',
-            'z-index': 9000
-        });
-}
-
-screenData();
-
-$(window).resize(function(){
-    screenData();
-});
-
 //==================================================
 // Developer: COMMAND+S for screen width
 //==================================================
