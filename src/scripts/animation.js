029450f88787374851fb7a6b83c825a595717dcc
Mitch <hello@liquidvisual.com>
2015-07-03 11:27:49 +1000

    After deploy

---
+++ src/scripts/animation.js
@@ -0,0 +1,29 @@
+/*
+    ANIMATION.JS - Last updated: 03.07.15
+
+    - Notes -
+
+    Uses jQuery Transit for animations
+*/
+//-----------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------
+
+var animationEnabled = $('.animation-enabled').length;
+
+//-----------------------------------------------------------------
+// Intro Animation
+//-----------------------------------------------------------------
+
+$(window).load(function() {
+
+	if (animationEnabled) {
+		$('.hero').transition({ opacity: 100, delay: 0 }, 2000);
+		$('.global-header').transition({ opacity: 100, delay: 1500 }, 1500);
+		$('.awards, .social-networks').transition({ opacity: 100, delay: 2000 }, 1500);
+	}
+});
+
+//==================================================
+//
+//==================================================
