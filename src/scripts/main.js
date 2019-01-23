3b4db25b8b5ca1d0b09e70f5952c3bc63c827ceb
liquidvisual <liquidvisual@gmail.com>
2019-01-23 13:06:59 +1100

    Interim minimal design for 2019

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,11 @@
 /*
-    MAIN SCRIPTS - Last updated: 00-00-00
+    MAIN SCRIPTS - Last updated: 23-01-19
 */
 //-----------------------------------------------------------------
-// Variables
+// VARIABLES
 //-----------------------------------------------------------------
-
-var TOUCH_ENABLED = $(".touch").length;
-
 //-----------------------------------------------------------------
-// Document Ready
+// READY
 //-----------------------------------------------------------------
 
 $(document).ready(function() {
@@ -18,38 +15,6 @@ $(document).ready(function() {
 $(window).load(function() {
     NProgress.done();
 });
-
-//-----------------------------------------------------------------
-// Kickstart Foundation / Touch Conditionals
-//-----------------------------------------------------------------
-
-$('#off-canvas-menu').mmenu({ "offCanvas": { "position": "right" }});
-
-var API = $("#off-canvas-menu").data("mmenu");
-
-$('.header-mobile-menu').bind('click', function(e) {
-    e.preventDefault();
-    API.open();
-});
-
-//-----------------------------------------------------------------
-// Fix 100vh in Mobile Safari
-// http://blog.rodneyrehm.de/archives/34-iOS7-Mobile-Safari-And-Viewport-Units.html
-//-----------------------------------------------------------------
-
-var IS_MOBILE_SAFARI = navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/);
-
-if (IS_MOBILE_SAFARI) {
-    var $element = $('.hero');
-
-    function fixMobileSafariViewport() {
-        $element.css('height', window.innerHeight);
-    }
-
-    // listen to portrait/landscape changes
-    window.addEventListener('orientationchange', fixMobileSafariViewport, true);
-    fixMobileSafariViewport();
-}
 
 //-----------------------------------------------------------------
 //
