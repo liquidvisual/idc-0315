7e2d2d21671c61919aaa8ed275dae5bf3b4bedcc
liquidvisual <liquidvisual@gmail.com>
2019-05-28 13:16:11 +1000

    Flatpickr integration
    * hopefully prevents issues with old jquery-ui implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,6 +10,25 @@
 
 $(document).ready(function() {
     NProgress.start(); // Start preloader bar
+
+    // Flatpickr - NEW 2019
+    $('.datepicker, .ui-datepicker').flatpickr({
+            format: "d-m-Y", // 2018-12-18
+            altFormat: "d-m-Y", // 06 Dec 2018 etc
+            altInput: true,
+            allowInput: true
+        }
+    );
+
+    // https://github.com/flatpickr/flatpickr/issues/892
+    const datePickers = document.querySelectorAll('.datepicker, .ui-datepicker');
+
+    datePickers.forEach(function(target) {
+        target.onkeypress = function() {
+            return false;
+        }
+    })
+
 });
 
 $(window).load(function() {
